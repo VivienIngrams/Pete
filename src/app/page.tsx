b06e8bd03b1d09d909183c6cf66eb599496aad7d
Vivien <vivingrams@gmail.com>
2025-10-21 16:16:20 +0100

    Trying to prevent dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,11 @@ import {
   type Post,
 } from '~/sanity/lib/sanity.queries'
 
+import { BannerWithAutoFallback } from './components/Banner'
 import NavMenu from './components/NavMenu'
 import PostsGrid from './series/PostsGrid'
 import PostsGridMobile from './series/PostsGridMobile'
+
 
 export default async function HomePage() {
   const client = getClient({ token: readToken })
@@ -36,48 +38,8 @@ export default async function HomePage() {
     <>
       <NavMenu />
 
-      {/* Fixed banner on top */}
-      <div
-        id="series-banner"
-        className="fixed top-[12vh] md:top-[9vw] left-0 right-0 z-20 bg-white"
-      >
-        <div className="hidden relative w-full  h-[13vh] md:flex items-center justify-center">
-          <Image
-            src="/shifting-ground.png"
-            alt="Shifting Ground"
-            fill
-            sizes="50vw"
-            className="object-contain"
-            priority
-          />
-        </div>
-        <div className="md:hidden relative w-full h-auto  flex flex-col items-center justify-center">
-          <div className="w-full h-[12vh] relative mr-5">
-            <Image
-              src="/shifting.png"
-              alt="Shifting"
-              fill
-              sizes="70vw"
-              className="object-contain"
-              priority
-            />
-          </div>
-          <div className="w-full h-[12vh] relative -mt-[4vh]">
-            <Image
-              src="/ground.png"
-              alt="Ground"
-              fill
-              sizes="70vw"
-              className="object-contain"
-              priority
-            />
-          </div>
-        </div>
+      <BannerWithAutoFallback />
 
-        <div className="pb-4 md:px-8 flex justify-center -mt-1 text-black font-light font-roboto tracking-wide text-lg">
-          <h3> Fine art photos for an upcoming book</h3>
-        </div>
-      </div>
 
       {/* Desktop horizontal scroll */}
       <div className="hidden md:block flex-grow h-full pl-12">
