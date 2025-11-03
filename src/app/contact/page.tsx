056a110c791170292bb7de68e1f15e81aec4802c
Vivien <vivingrams@gmail.com>
2025-11-03 16:37:49 +0000

    ADd mailto

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,25 +1,29 @@
-import { cookies } from 'next/headers';
+import { cookies } from 'next/headers'
 
-import ContactForm from '../components/ContactForm';
-import NavMenu from '../components/NavMenu';
+import ContactForm from '../components/ContactForm'
+import NavMenu from '../components/NavMenu'
+import Link from 'next/link'
 
 export default function ContactPage() {
-  const cookieStore = cookies();
-  const language = cookieStore.get('language')?.value || 'fr';
- 
+  const cookieStore = cookies()
+  const language = cookieStore.get('language')?.value || 'fr'
+
   return (
     <>
-    <NavMenu/>
+      <NavMenu />
 
-    <div className="min-h-screen bg-black dark:bg-black xl:h-[90%] flex flex-col items-center justify-center">
-      <div className=" mx-6 xl:w-1/3 xl:mx-auto">
-        <h1 className="text-2xl xl:text-3xl  w-full text-left font-light uppercase tracking-widest pt-24 md:pt-12 pb-8">
-          Contact
-        </h1>
-      
-        <ContactForm  />
+      <div className="min-h-screen bg-black dark:bg-black xl:h-[90%] flex flex-col items-center justify-center">
+        <div className=" mx-6 xl:w-1/3 xl:mx-auto">
+          <h1 className="text-2xl xl:text-3xl  w-full text-left font-light uppercase tracking-widest pt-24 md:pt-12 pb-8">
+            Contact
+          </h1>
+
+          <ContactForm />
+          <div className="w-full text-center">
+            <p>Or write to us at <Link className="underline" href="mailto:studiolippmann@gmail.com" >studiolippmann@gmail.com </Link> </p>
+          </div>
+        </div>
       </div>
-    </div>
     </>
-  );
+  )
 }
