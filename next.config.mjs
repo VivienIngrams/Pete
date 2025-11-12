a6de8de0653881d4c24e78110f05db4e8ca08f83
Vivien <vivingrams@gmail.com>
2025-11-12 23:50:14 +0000

    unoptimize images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
  images: {
-  
+  unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
